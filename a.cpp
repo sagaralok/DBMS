8dcff832dc1fef75577517f5b3a2e7191ffa1365
sagaralok <sagaralok365@gmail.com>
2022-04-27 05:10:41 +0530

    Basics of SQL

---
+++ a.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define loop(x,a,n) for(long long int x=a ;x<n;x++)
+#define ip cin>>
+#define vl vector<ll>
+#define vvi vector<vl>
+#define vp vector<pair<ll,ll>>
+#define pb(a) push_back(a)
+#define mp(a,b) make_pair(a,b)
+#define mpl map<ll,ll>
+#define MOD 1000000007
+#define arrL 1000009
+#define op cout<<
+#define ope(x) cout<<x<<endl;
+#define lpair pair<ll,ll>
+#define cont continue 
+
+
+int main(){
+    fast
+    ll t=1;
+    ip t;
+    while(t--){
+        ll n;ip n;
+        ll a[n];
+        loop(i,0,n){
+            ip a[i];
+        }
+        
+    }
+}
